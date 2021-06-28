34ff8827fa7a8fc24723098ef7d15726a746343b
AhmedAhmedFekry <ahmedahmedfekry11305654@gmail.com>
2021-06-28 16:32:21 +0200

    template make extends, include

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,6 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-
 # Create your views here.
 
 def index(request):
-    return HttpResponse('this test index ')
+    return render(request,'dashboard/index.html',{})
