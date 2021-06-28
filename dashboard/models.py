2ce92556a85290fbc546caf78ded630dd1f61a07
AhmedAhmedFekry <ahmedahmedfekry11305654@gmail.com>
2021-06-28 18:55:00 +0200

    create product and category model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    
+
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(null=False, unique=True, allow_unicode=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+        
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    quantity = models.PositiveIntegerField(null=True)
+    category = models.ForeignKey(
+        Category,  related_name="categoryProduct", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}'
