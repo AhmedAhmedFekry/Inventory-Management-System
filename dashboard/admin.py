3d66727781f50446a5e0fb86c501334672e02f93
AhmedAhmedFekry <ahmedahmedfekry11305654@gmail.com>
2021-06-29 13:30:15 +0200

    create order table

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product, Category ,Order
+
+
+# Register your models here.
+admin.site.register(Category )
+admin.site.register(Product)
+admin.site.register(Order)
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Category(models.Model):
@@ -21,3 +21,11 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+
+class Order(models.Model):
+    name = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    order_quantity = models.PositiveIntegerField(null=True)
+
+    def __str__(self):
+        return f'{self.customer}-{self.name}'
